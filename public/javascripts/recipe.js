5d8ef44acb5bdd19dcf764cc3a2858fc08d94c9b
samyoshon <samuelyshon@gmail.com>
2015-10-07 23:20:51 -0700

    front page design, single search

--- public/javascripts/recipe.js
+++ public/javascripts/recipe.js
@@ -1,5 +1,5 @@
 var RECIPES = [];
-var recipeTemplate = _.template('<a href="<%= url %>"><div class="single-recipe"><div class="row"><div class="col-lg-8"><div class="videoDiv"><video class="foodGif" width="100%" loop="true"><source src="<%= recipe.main_image_url %>" type="video/webm">Your browser does not support the video tag.</video></div></div><div class="col-lg-4" style="padding: 0px 20px 10px 20px;"><h1><%= recipe.name %></h1><p style="background: #446688; padding: 10px; color: #fff;"><%= recipe.description %></p><br/><p>Prep Time: <%= recipe.prep_time %> min</p><p>Cook Time: <%= recipe.cook_time %> min</p></div></div></div></a>');
+var recipeTemplate = _.template('<a href="<%= url %>"><div class="single-recipe"><div class="row"><div class="col-lg-8"><div class="videoDiv"><video class="foodGif" width="100%" loop="true"><source src="<%= recipe.main_image_url %>" type="video/webm">Your browser does not support the video tag.</video></div></div><div class="col-lg-4" style="padding: 0px 20px 10px 20px;"><h1><%= recipe.name %></h1><h2 id="recipe-index-description"><%= recipe.description %></h2><br/><p>Prep Time: <%= recipe.prep_time %> min</p><p>Cook Time: <%= recipe.cook_time %> min</p></div></div></div></a>');
 
 $(document).ready(function() {
 	// Get dem donuts!
@@ -30,61 +30,33 @@ $(document).ready(function() {
 
 			e.preventDefault();
 			var value = $('#search-term').val().toLowerCase().toString();
-			console.log("Search term: " + value);
-			emptyRecipes();
-
-			var resultRecipes = RECIPES;
-
-			var queryCollection = [];
-				
-			resultRecipes.forEach (function (recipe) {
-				var resultIngredients = recipe.ingredients;
-
-				resultIngredients.forEach(function(ingredient) {
-					var ingredientName = ingredient.name.toLowerCase();
-					console.log("Ingredient: " + ingredientName);
-					if (ingredientName.indexOf(value) > -1) {
-						if (queryCollection.indexOf(recipe) == -1) {
-							queryCollection.push(recipe);
-						}
-					}
-				});
-			});
-
-			queryCollection.forEach(function(recipe) {
-				console.log("recipe: " + recipe);
-				var processed_url = "http://localhost:3000/recipes/" + recipe.name.replace(/ /g, "_");
-				var compiled = recipeTemplate({
-				recipe: recipe,
-				url: processed_url
-			});
-				recipesContainer.append(compiled);
-			});
-		});
-
-		$('#allergy-button').click(function(e) {
-
-			e.preventDefault();
-			var value = $('#search-term').val().toLowerCase().toString();
 			var allergy = $('#allergy-term').val().toLowerCase().toString();
-			console.log("Search term: " + value);
-			console.log("Allergy term: " + allergy);
-
+			console.log(value);
+			console.log(allergy);
 			emptyRecipes();
 
 			var resultRecipes = RECIPES;
 
 			var queryCollection = [];
-				
+			
 			resultRecipes.forEach (function (recipe) {
 				var resultIngredients = recipe.ingredients;
 
+			
 				resultIngredients.forEach(function(ingredient) {
 					var ingredientName = ingredient.name.toLowerCase();
 					console.log("Ingredient: " + ingredientName);
-					if ((ingredientName.indexOf(value) > -1) && (ingredientName.indexOf(allergy) < -1)){
-						if (queryCollection.indexOf(recipe) == -1) {
-							queryCollection.push(recipe);
+					if (!allergy) {
+						if (ingredientName.indexOf(value) > -1) {
+							if (queryCollection.indexOf(recipe) == -1) {
+								queryCollection.push(recipe);
+							}
+						}
+					} else {
+						if ((ingredientName.indexOf(value) > -1) && (ingredientName.indexOf(allergy) < -1)) {
+							if (queryCollection.indexOf(recipe) == -1) {
+								queryCollection.push(recipe);
+							}
 						}
 					}
 				});
