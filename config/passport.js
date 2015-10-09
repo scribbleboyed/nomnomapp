55b36687cf5fec3675c4b0823414cf6bc68e702b
samyoshon <samuelyshon@gmail.com>
2015-10-08 20:12:31 -0700

    front page styled perfect

--- config/passport.js
+++ config/passport.js
@@ -56,3 +56,101 @@
 // 		}
 // 	));
 // };
+	// <div class="row template-one-row template-container">
+	// 	<% if (recipe[0]) { %>
+	// 	<a href="<%= processed_url[0] %>">
+	// 	<div class="col-lg-8 big-video-col" id="big-video-col">
+	// 		<div class="videoDiv">
+	// 			<video autoplay class="foodGif" width="100%" loop="true" >
+	// 				<source src="<%= recipe[0].main_image_url %>" type="video/webm">Your browser does not support the video tag.
+	// 			</video>
+	// 		</div>
+	// 		<div class="videoOverlay" id="big-videoOverlay">
+	// 				<h1 class="bv-title"><%= recipe[0].name %></h1>
+	// 			<div id="videoOverlay-timer">
+	// 				<h2 class="bv">Prep Time: <%= recipe[0].prep_time %></h2>
+	// 				<h2 class="bv">Cook Time: <%= recipe[0].cook_time %></h2>
+	// 			</div>
+	// 		</div>
+	// 	</div>
+	// 	</a>
+	// 	<% } %>
+	// 	<% if (recipe[1]) { %>
+	// 	<a href="<%= processed_url[1] %>">
+	// 	<div class="col-lg-4 small-video-col" id="small-video-col">
+	// 		<div class="small-video top" id="small-video-top">
+	// 			<div class="videoDiv" id="sv-top-videoDiv">
+	// 				<video autoplay class="foodGif" width="100%" loop="true"><source src="<%= recipe[1].main_image_url %>" type="video/webm">Your browser does not support the video tag.</video>
+	// 			</div>
+	// 			<div class="videoOverlay" id="sv-top-videoOverlay">
+	// 				<h2 class="sv"><%= recipe[1].name %></h2>
+	// 			</div>
+	// 		</div>
+	// 	</a>
+	// 	<% } %>
+	// 	<% if (recipe[2]) { %>
+	// 	<a href="<%= processed_url[2] %>">
+	// 		<div class="small-video bottom" id="small-video-bot">
+	// 			<div class="videoDiv" id="sv-bot-videoDiv">
+	// 				<video autoplay class="foodGif" width="100%" loop="true"><source src="<%= recipe[2].main_image_url %>" type="video/webm">Your browser does not support the video tag.</video>
+	// 			</div>
+	// 			<div class="videoOverlay" id="sv-bot-videoOverlay">
+	// 				<h2 class="sv"><%= recipe[2].name %></h2>
+	// 			</div>
+	// 		</div>
+	// 	</div>
+	// 	</a>
+	// 	<% } else { %>
+	// 	</div>
+	// 	<% } %>
+	// </div>
+
+	<!-- // TEMPLATE TWO -->
+
+	// <div class="row template-two-row template-container">
+	// 	<% if (recipe[0]) { %>
+	// 	<a href="<%= processed_url[0] %>">
+	// 	<div class="col-lg-4 small-video-col" id="small-video-col">
+	// 		<div class="small-video top" id="small-video-top">
+	// 			<div class="videoDiv" id="sv-top-videoDiv">
+	// 				<video autoplay class="foodGif" width="100%" loop="true"><source src="<%= recipe[0].main_image_url %>" type="video/webm">Your browser does not support the video tag.</video>
+	// 			</div>
+	// 			<div class="videoOverlay" id="sv-top-videoOverlay">
+	// 				<h2 class="sv"><%= recipe[0].name %></h2>
+	// 			</div>
+	// 		</div>
+	// 	</a>
+	// 	<% } %>
+	// 	<% if (recipe[1]) { %>
+	// 	<a href="<%= processed_url[1] %>">
+	// 		<div class="small-video bottom" id="small-video-bot">
+	// 			<div class="videoDiv" id="sv-bot-videoDiv">
+	// 				<video autoplay class="foodGif" width="100%" loop="true"><source src="<%= recipe[1].main_image_url %>" type="video/webm">Your browser does not support the video tag.</video>
+	// 			</div>
+	// 			<div class="videoOverlay" id="sv-bot-videoOverlay">
+	// 				<h2 class="sv"><%= recipe[1].name %></h2>
+	// 			</div>
+	// 		</div>
+	// 	</div>
+	// 	</a>
+	// 	<% } else { %>
+	// 	</div>
+	// 	<% } %>
+	// 	<% if (recipe[2]) { %>
+	// 	<a href="<%= processed_url[2] %>">
+	// 	<div class="col-lg-8 big-video-col" id="big-video-col">
+	// 		<div class="videoDiv">
+	// 			<video autoplay class="foodGif" width="100%" loop="true"><source src="<%= recipe[2].main_image_url %>" type="video/webm">Your browser does not support the video tag.</video>
+	// 		</div>
+	// 		<div class="videoOverlay" id="big-videoOverlay">
+	// 				<h1 class="bv-title"><%= recipe[2].name %></h1>
+	// 			<div id="videoOverlay-timer">
+	// 				<h2 class="bv">Prep Time: <%= recipe[2].prep_time %></h2>
+	// 				<h2 class="bv">Cook Time: <%= recipe[2].cook_time %></h2>
+	// 			</div>
+	// 		</div>
+	// 	</div>
+	// 	</a>
+	// 	<% } %>
+	// </div>
+
